e1c8ee5c6547ebf13c75c99aac0e1710cbd8c368
Luke Van Jelgerhuis <112013811+lvanjelg@users.noreply.github.com>
2024-01-29 21:52:50 -0500

    Update InventoryControllerTest.java

--- src/test/java/edu/iu/lvanjelg/c322spring2024homework2/controllers/InventoryControllerTest.java
+++ src/test/java/edu/iu/lvanjelg/c322spring2024homework2/controllers/InventoryControllerTest.java
@@ -1,7 +1,21 @@
 package edu.iu.lvanjelg.c322spring2024homework2.controllers;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryControllerTest {
 
+    @Test
+    void search() {
+
+    }
+
+    @Test
+    void getGuitar() {
+    }
+
+    @Test
+    void addGuitar() {
+    }
 }
